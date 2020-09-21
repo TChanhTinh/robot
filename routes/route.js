c6d30a68983ccde74ede40e06ec688c771b216e4
TChanhTinh <65092713+TChanhTinh@users.noreply.github.com>
2020-09-21 09:36:11 +0700

    Update route.js
    [Tchanhtinh] Add template render api

--- routes/route.js
+++ routes/route.js
@@ -2,4 +2,13 @@ module.exports = (app) => {
  app.get("/", (req, res) => {
     res.send("hello world!")
  })
+ 
+ app.post("word/{para}", (req, res) => {
+  const page
+  db.words.findOne({name: req.params}, (err, doc) => {
+   if err 
+    console.log(err)
+   page = renderWord(doc)
+ }) 
+ res.render(page)
 }
