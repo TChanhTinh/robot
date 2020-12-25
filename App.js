804e0c22d24a964f97a36578ece2863a42ab4a18
tchanhtinh <tchanhtinh@gmail.com>
2020-12-25 15:58:52 +0700

    Remade the UI

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useLayoutEffect, useState } from 'react';
 import Axios from 'axios';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Modal, Dimensions } from 'react-native';
 import { SearchBar } from 'react-native-elements';
 
 import * as SQLite from 'expo-sqlite'
@@ -10,6 +10,7 @@ const db = SQLite.openDatabase('db.veterinar', "v1", "App",  5 * 1024 * 1024) //
 
 export default function App() {
   const [lastSync, setLastSync] = useState({id: 0, times: "1999-1-1"})
+  const [modalVisible, setModalVisible] = useState(false)
   const [word, setWord] = useState('')
   const [wordData, setWordData] = useState({word: "", type: "", mean: "", pronunciation: "", description: ""})
 
@@ -85,9 +86,14 @@ export default function App() {
   function submitSearch(input) {
     db.transaction(tx => {
       tx.executeSql(`SELECT * FROM veterinary_husbandry WHERE lower(word)=lower('${input}')`, null,
-      (txObj, resultSet) => {console.log(resultSet.rows._array[0]); resultSet.rows._array[0] != null ? setWordData(resultSet.rows._array[0]) : {} },
+      (txObj, resultSet) => { resultSet.rows._array[0] != null ? setWordData(resultSet.rows._array[0]) : popupModal() },
       (txObj, err) => console.log(err))
     })
+  }
+
+  function popupModal() {
+    setModalVisible(true)
+    setTimeout(() => {setModalVisible(false)}, 2000)
   }
 
   useLayoutEffect(() => {  
@@ -106,17 +112,51 @@ export default function App() {
   }, [])
 
   return (
-    <View style={{padding: 10}}>
-      <TextInput
-        style={{height: 40}}
-        placeholder="Type here to translate!"
-        onChangeText={e => updateSearch(e)}
-        onSubmitEditing={() => {console.log(word) ;submitSearch(word)}}
-        defaultValue={word}
-      />
-      {Object.values(wordData).map((mapData) => (
-        <Text>{mapData}</Text>
-      ))}
+    <View style={{flex:1, backgroundColor: "#DDDDDD", overflow: "scroll"}}>
+      <View style={{backgroundColor: '#93B5E1'}}>
+        <Text style={{padding: 40, paddingBottom: 10, alignSelf: 'center', fontSize: 25, fontWeight: 'bold'}}>
+          Veterinary Dictionary
+        </Text>
+      </View>
+      <View>
+        <SearchBar
+          style={{height: 40}}
+          platform="ios"
+          placeholder="Type here to translate!"
+          onChangeText={e => updateSearch(e)}
+          onSubmitEditing={() => {console.log(word); submitSearch(word)}}
+          value={word}
+          containerStyle={{backgroundColor: "#93B5E1"}}
+        />
+      </View>
+      <ScrollView style={{padding: 15}}>
+        {Object.entries(wordData).map(([key, value]) => (
+          <View style={{ marginBottom: 10 }}>
+            <Text style={{fontWeight: "bold", alignSelf: "flex-start", padding: 6, borderWidth: 1, borderColor: "#89C9B8", borderRadius: 10}}>
+              {key.toUpperCase()}
+            </Text>
+            <Text style={{alignSelf: "center", padding: 5, fontSize: 19}}>{value}</Text>
+          </View> 
+        ))}
+      </ScrollView>
+      <Modal visible={modalVisible} transparent={true} animationType="fade">
+        <View onTouchStart={() => setModalVisible(false)}
+        style={{ backgroundColor: "black", zIndex: 1, opacity: 0.2, flex: 1, justifyContent: "center", alignItems: "center"}}>
+        </View> 
+          <View style={{
+              padding: 20,
+              zIndex: 2,
+              maxWidth: Dimensions.get('window').width/2,
+              maxHeight: Dimensions.get('window').height/2,
+              backgroundColor: "white",
+              position: "absolute",
+              right: Dimensions.get('window').width/2 - 85,
+              bottom: Dimensions.get('window').height/2 - 30,
+              borderRadius: 20}}>
+            <Text style={{ fontSize: 16, fontWeight: "bold"}}>Word not existed!</Text>
+          </View>
+        
+      </Modal>
       <StatusBar style="auto" />
     </View>
   );
